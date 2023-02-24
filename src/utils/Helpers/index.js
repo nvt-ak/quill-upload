499b98883559e0b5c652494d24a9ec0ad14ff1fb
Michael Möhler <michael.moehler@rhein-spree.com>
2023-02-24 14:23:36 +0100

    Fix video image

--- src/utils/Helpers/index.js
+++ src/utils/Helpers/index.js
@@ -23,30 +23,29 @@ export default {
   },
   videoIconHTML: function() {
     return `<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect width="18" height="18" fill="#1E1E1E"/>
-              <g clip-path="url(#clip0_0_1)">
-              <rect width="18" height="18" fill="white"/>
-              <path d="M15 3H3V15H15V3Z" fill="black"/>
-              <path d="M6 3H5V15H6V3Z" fill="black"/>
-              <path d="M13 3H12V15H13V3Z" fill="black"/>
-              <path d="M13 8H5V10H13V8Z" fill="black"/>
-              <path d="M6 5H3V6H6V5Z" fill="black"/>
-              <path d="M6 7H3V8H6V7Z" fill="black"/>
-              <path d="M6 10H3V11H6V10Z" fill="black"/>
-              <path d="M6 12H3V13H6V12Z" fill="black"/>
-              <path d="M15 5H12V6H15V5Z" fill="black"/>
-              <path d="M15 7H12V8H15V7Z" fill="black"/>
-              <path d="M15 10H12V11H15V10Z" fill="black"/>
-              <path d="M15 12H12V13H15V12Z" fill="black"/>
-              <rect x="10" y="12" width="4" height="5" fill="white"/>
-              <path d="M14.7963 11.8761L14.7872 12.0848H14.8V12.498V12.698H15C15.9641 12.698 16.7492 13.4541 16.7981 14.4035L16.8 14.498C16.799 15.4594 16.0432 16.2452 15.0928 16.2923L14.9961 16.2942L14.8 16.298V16.298H12.7V14.7371H13.7324H14.1653L13.8847 14.4075L12.1523 12.3723L11.9997 12.193L11.8474 12.3726L10.1209 14.4097L9.84175 14.7391H10.2734H11.3V16.3H9.00031C8.00467 16.2963 7.2 15.4907 7.2 14.498C7.2 13.6284 7.81854 12.902 8.63948 12.735L8.87189 12.6878L8.78605 12.4667C8.73054 12.3237 8.7 12.1666 8.7 12.002C8.7 11.2843 9.28233 10.702 10 10.702C10.2389 10.702 10.4623 10.7662 10.6557 10.8782L10.8361 10.9826L10.933 10.798C11.2748 10.1464 11.96 9.7 12.75 9.7C13.8818 9.7 14.8 10.6184 14.8 11.748C14.8 11.788 14.7983 11.8307 14.7963 11.8761Z" fill="white" stroke="black" stroke-width="0.4"/>
-              </g>
-              <defs>
-              <clipPath id="clip0_0_1">
-              <rect width="18" height="18" fill="white"/>
-              </clipPath>
-              </defs>
-              </svg>`
+    <g clip-path="url(#clip0_1_2)">
+    <rect width="18" height="18" fill="white"/>
+    <path d="M3.5 3.5H14.5V14.5H3.5V3.5Z" stroke="black"/>
+    <path d="M6 3H5V15H6V3Z" fill="black"/>
+    <path d="M13 3H12V15H13V3Z" fill="black"/>
+    <path d="M13 8H5V10H13V8Z" fill="black"/>
+    <path d="M6 5H3V6H6V5Z" fill="black"/>
+    <path d="M6 7H3V8H6V7Z" fill="black"/>
+    <path d="M6 10H3V11H6V10Z" fill="black"/>
+    <path d="M6 12H3V13H6V12Z" fill="black"/>
+    <path d="M15 5H12V6H15V5Z" fill="black"/>
+    <path d="M15 7H12V8H15V7Z" fill="black"/>
+    <path d="M15 10H12V11H15V10Z" fill="black"/>
+    <path d="M15 12H12V13H15V12Z" fill="black"/>
+    <rect x="8.2" y="10.6" width="5.6" height="7" fill="white"/>
+    <path d="M14.9947 10.43L14.9856 10.6387H15V11.2973V11.4973H15.2C16.593 11.4973 17.7273 12.59 17.7974 13.9621L17.8 14.0965C17.799 15.486 16.7065 16.6218 15.3327 16.6892L15.1961 16.6918L15 16.6957V16.6973H11.9V14.352H13.4254H13.8583L13.5777 14.0223L11.1523 11.1731L10.9997 10.9938L10.8474 11.1734L8.43024 14.0254L8.15113 14.3547H8.58281H10.1V16.7L6.80071 16.7C6.80062 16.7 6.80053 16.7 6.80044 16.7C5.36255 16.6948 4.2 15.5314 4.2 14.0973C4.2 12.8409 5.09354 11.7919 6.27932 11.5507L6.51173 11.5034L6.4259 11.2823C6.3446 11.0729 6.3 10.8432 6.3 10.6027C6.3 9.55382 7.15108 8.70273 8.2 8.70273C8.54908 8.70273 8.87561 8.79671 9.158 8.96019L9.33845 9.06467L9.43531 8.88002C9.92712 7.94251 10.9131 7.3 12.05 7.3C13.6787 7.3 15 8.62151 15 10.2473C15 10.3052 14.9975 10.3666 14.9947 10.43Z" fill="white" stroke="black" stroke-width="0.4"/>
+    </g>
+    <defs>
+    <clipPath id="clip0_1_2">
+    <rect width="18" height="18" fill="white"/>
+    </clipPath>
+    </defs>
+    </svg>`
   },
   attachmentHTML: function () {
     return `<a href="#" target="_blank" download>[Speichert ..]</a>`;
